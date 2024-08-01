60de01cfc98919f079e616dc73e36036e1720088
Tau <taumasaire@gmail.com>
2024-08-01 19:21:09 +0200

    updating import statements

--- __init__.py
+++ __init__.py
@@ -2,15 +2,14 @@
 import click
 import os
 from flask import Flask, g
-from .config import Config
+from config import Config
 from flask.cli import with_appcontext
 from flask_mail import Mail
-from flask_login import LoginManager
 from markupsafe import Markup
 import logging
 from logging.handlers import RotatingFileHandler
-from .dbase import db
-from .models import Comment, PostTag, Tag, User
+from dbase import db
+from models import Comment, PostTag, Tag, User
 
 # Initialize mail
 mail = Mail()
@@ -21,7 +20,7 @@ def nl2br(value):
     return Markup(value.replace('\n', '<br>\n'))
 
 def get_db():
-    """Initializes a database connection."""
+    """Initializes a database connection"""
     if 'db' not in g:
         g.db = db
     return g.db
@@ -44,9 +43,6 @@ def init_db_command():
     init_db()
     click.echo('Initialized the database.')
 
-# Initialize the login manager 
-login_manager = LoginManager()
-
 def create_app():
     """Create and configure the Flask application.
 
@@ -55,8 +51,6 @@ def create_app():
     """
     # Create and configure the Flask application
     app = Flask(__name__, instance_relative_config=True)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'  # The name of the view to redirect to when the user needs to log in.
 
     # Load the default configuration
     app.config.from_object(Config)
@@ -74,7 +68,7 @@ def create_app():
     db.init_app(app)
 
     # Register blueprints
-    from . import auth, blog
+    import auth, blog
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='blog.index')
--- auth.py
+++ auth.py
@@ -2,14 +2,15 @@
 import functools
 import re
 import os
-from .dbase import db
+from dbase import db
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify, current_app as app
 from flask_mail import Message
 from flask_login import login_user, logout_user, login_required, current_user
-from . import login_manager
-from . import mail
-from .models import User
+from flask_login import LoginManager
+login_manager = LoginManager()
+from __init__ import mail
+from models import User
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -19,21 +20,8 @@ s = URLSafeTimedSerializer('Thisisasecret!')
 
 @bp.route('/signup', methods=('GET', 'POST'))
 def signup():
-    """
-    Registers a new user.
-
-    If the request method is POST, it handles the form submission.
-    It checks if the username, email, and password are provided.
-    It also validates the email format
-    If the passwords do not match, it sets an error message.
-    If no error occurs, it checks if the username or email already exist in the database.
-    If not, it creates a new user object and adds it to the database.
-    Finally, it redirects to the login page.
-
-    If the request method is GET, it renders the signup.html template.
-    """
+    """Registers a new user"""
     if request.method == 'POST':
-        # Get form data
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
@@ -43,7 +31,6 @@ def signup():
         # Email validation regex
         email_regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-        # Check if required fields are provided
         if not username:
             error = 'Username is required.'
         elif not email:
@@ -56,7 +43,7 @@ def signup():
             error = 'Passwords do not match.'
 
         if error is None:
-            # Check if the username or email already exist
+            # Check if the username or email already exists
             existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
             if existing_user:
                 error = f"User {username} or email {email} is already registered."
@@ -69,7 +56,6 @@ def signup():
 
         flash(error)
 
-    # Render the signup.html template
     return render_template('signup.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
@@ -80,6 +66,7 @@ def login():
         password = request.form.get('password')
         error = None
 
+
         if not email or not password:
             error = 'Email and password are required.'
         else:
@@ -87,7 +74,7 @@ def login():
                 user = User.query.filter_by(email=email).first()
             except Exception as e:
                 error = 'Database error occurred.'
-                
+
             if user is None:
                 error = 'Incorrect email or password.'
             elif not check_password_hash(user.password, password):
@@ -101,116 +88,68 @@ def login():
 
     return render_template('login.html')
 
-
 @bp.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
-    """
-    Handles the forgot password form submission.
-
-    If the request method is GET, it renders the forgot_password.html template.
-    If the request method is POST, it validates the email and sends a password reset link.
-
-    Returns:
-        If the request method is GET, it renders the forgot_password.html template.
-        If the request method is POST and the email is not provided, it returns a JSON response with an error message.
-        If the request method is POST and the email does not exist in the database, it returns a JSON response with an error message.
-        If the request method is POST, it generates a token and sends a password reset link via email.
-        It returns a JSON response with a success message.
-    """
     if request.method == 'GET':
         return render_template('forgot_password.html')
 
-    # Get email from form data
     email = request.form.get('email')
 
-    # Check if email is provided
     if not email:
         return jsonify({'message': 'Email is required.'}), 400
 
-    # Verify if the email exists in the database
+    # Verify if the email exists in your database
     user = db.session.query(User).filter_by(email=email).first()
 
-    # Check if the email exists in the database
     if not user:
         return jsonify({'message': 'Email does not exist.'}), 400
 
-    # If the email exists, generate a token and send a password reset link
+    # If it does, proceed to generate a token and send the email
     token = s.dumps(email, salt='email-confirm-salt')
 
-    # Send password reset link via email
     msg = Message('Password Reset Request', sender=os.getenv('MAIL_USERNAME'), recipients=[email])
     link = url_for('auth.reset_password', token=token, _external=True)
     msg.body = 'Your link to reset your password is {}'.format(link)
     mail.send(msg)
 
-    # Return a success message
     return jsonify({'message': 'Please check your email for a password reset link.'}), 200
 
 @bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
-    """
-    Handles the password reset form submission.
-
-    If the request method is GET, it renders the reset_password.html template.
-    If the request method is POST, it validates the password and updates the user's password in the database.
-
-    Returns:
-        If the request method is GET, it renders the reset_password.html template.
-        If the request method is POST and the password is not provided, it returns a JSON response with an error message.
-        If the request method is POST, it updates the user's password and returns a JSON response with a success message.
-    """
     try:
-        # Load the email from the token
         email = s.loads(token, salt='email-confirm-salt', max_age=3600)
     except SignatureExpired:
-        # Return an error message if the token has expired
         return jsonify({'message': 'The password reset link is expired.'}), 400
 
-    # Query the user by email
     user = db.session.query(User).filter_by(email=email).first()
 
-    # Return an error message if the user does not exist
     if user is None:
         return jsonify({'message': 'Email does not exist.'}), 400
 
     if request.method == 'GET':
-        # Render the reset_password.html template
         return render_template('reset_password.html', email=email, token=token)
-    
-    if request.method == 'POST':
-        # Get the new password from the form data
-        new_password = request.form.get('password')
 
-        # Return an error message if the password is not provided
+    if request.method == 'POST':
+        new_password = request.form.get('password')
         if new_password is None:
             return jsonify({'message': 'Password is required.'}), 400
 
-        # Hash the new password
         hashed_password = generate_password_hash(new_password)
-
-        # Update the user's password
         user.password = hashed_password
         db.session.commit()
 
-        # Return a success message
+        # Notify user
         return jsonify({'message': 'Password has been reset successfully, please login.'}), 200
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    """Loads the user object from the database."""
+    """Loads the user object from the database"""
     return User.query.get(int(user_id))
 
 @bp.route('/logout')
 @login_required
 def logout():
-    """
-    Logs the user out by clearing the session and redirecting to the blog index page.
-
-    Returns:
-        redirect: Redirects to the blog index page.
-    """
-    # Clear the session to log the user out
+    """logs the user out"""
     logout_user()
-    
-    # Redirect to the blog index page
     return redirect(url_for('blog.index'))
